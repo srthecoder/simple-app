3f32cd539b04a4ccb2f688a8fef9578894a03bfc
idor Student1 <idorstudent1@lib.usc.edu>
2024-07-25 15:20:15 -0700

    adding download link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, url_for
 import os
 
 app = Flask(__name__)
@@ -16,11 +16,6 @@ def index():
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
-    <h1>Download File</h1>
-    <form method=get action="/download">
-      <input type=text name=filename>
-      <input type=submit value=Download>
-    </form>
     '''
 
 @app.route('/upload', methods=['POST'])
@@ -33,12 +28,58 @@ def upload_file():
     if file:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
-        return 'File uploaded successfully'
+        download_link = url_for('download_file', filename=file.filename, _external=True)
+        return f'File uploaded successfully. Download link: <a href="{download_link}">{download_link}</a>'
 
-@app.route('/download', methods=['GET'])
-def download_file():
-    filename = request.args.get('filename')
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run()
+
+
+# from flask import Flask, request, send_from_directory
+# import os
+
+# app = Flask(__name__)
+# UPLOAD_FOLDER = '/tmp/uploads'  # Use /tmp directory in serverless environments
+# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# @app.route('/')
+# def index():
+#     return '''
+#     <!doctype html>
+#     <title>Upload File</title>
+#     <h1>Upload File</h1>
+#     <form method=post enctype=multipart/form-data action="/upload">
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     <h1>Download File</h1>
+#     <form method=get action="/download">
+#       <input type=text name=filename>
+#       <input type=submit value=Download>
+#     </form>
+#     '''
+
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     if 'file' not in request.files:
+#         return 'No file part'
+#     file = request.files['file']
+#     if file.filename == '':
+#         return 'No selected file'
+#     if file:
+#         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+#         file.save(filepath)
+#         return 'File uploaded successfully'
+
+# @app.route('/download', methods=['GET'])
+# def download_file():
+#     filename = request.args.get('filename')
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+# if __name__ == '__main__':
+#     app.run()
