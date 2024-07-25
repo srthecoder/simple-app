c95b5bf69604934a1766a6500204a17d4cbde0b0
idor Student1 <idorstudent1@lib.usc.edu>
2024-07-25 16:13:18 -0700

    change 6

--- app.py
+++ app.py
@@ -12,15 +12,18 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
+    if 'marcFile' not in request.files or 'outputName' not in request.form:
+        return 'No file part or output name'
+    file = request.files['marcFile']
+    output_name = request.form['outputName']
     if file.filename == '':
         return 'No selected file'
     if file:
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file_ext = os.path.splitext(file.filename)[1]
+        output_filename = output_name + file_ext
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
         file.save(filepath)
-        download_link = url_for('download_file', filename=file.filename, _external=True)
+        download_link = url_for('download_file', filename=output_filename, _external=True)
         return f'File uploaded successfully. Download link: <a href="{download_link}">{download_link}</a>'
 
 @app.route('/download/<filename>', methods=['GET'])
@@ -29,7 +32,6 @@ def download_file(filename):
 
 if __name__ == '__main__':
     app.run()
-
 
 
 
